8f8ba0d61ac70eef990bd536cc1c98283e7ce3ac
Damian Martinez <DCAM.DAMIAN.DM@GMAIL.COM>
2026-01-29 14:26:35 -0700

    update: tables created w/ no errors.

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,21 @@
--- TODO
+DROP TABLE IF EXISTS playlist_tracks;
+DROP TABLE IF EXISTS tracks;
+DROP TABLE IF EXISTS playlist;
+
+CREATE TABLE playlists (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE tracks (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  duration_ms INTEGER NOT NULL
+);
+
+CREATE TABLE playlists_tracks (
+  id SERIAL PRIMARY KEY,
+  playlist_id INTEGER NOT NULL REFERENCES playlists(id) ON DELETE CASCADE,
+  track_id INTEGER NOT NULL REFERENCES tracks(id) ON DELETE CASCADE
+);
